36ed92398947942ffa1eac25279e08e289d48136
Hera <mostafizhera@gmail.com>
2024-12-11 15:56:04 +0600

    jobs portal project all setup completed

---
+++ src/Router/router.jsx
@@ -0,0 +1,19 @@
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../HomePage/Home";
+
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout />,
+      children: [
+        { path: "/", element: <Home /> },
+      ],
+    },
+  ]);
+
+export default router;
