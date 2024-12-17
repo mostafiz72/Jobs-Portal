d447f6346d397dac60ac014085e51ba42bbaffb1
Hera <mostafizhera@gmail.com>
2024-12-18 01:22:23 +0600

    added the winers user

--- src/Components/ViewAllApplication/ViewApplication.jsx
+++ src/Components/ViewAllApplication/ViewApplication.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import { useLoaderData } from 'react-router-dom'
+import Swal from 'sweetalert2';
 
 export default function ViewApplication() {
   const viewApplications = useLoaderData();
 
   const handleStatusUpdate = (e, id) => {  // amaa ekhane duita parameter nisi amra kare winer dilam tar to id numbner lagbe tai id number neuya hoyse
-    console.log(e.target.value, id);
-    
+
+    /// send the data in the server ----------------
+    const data = {
+      status: e.target.value  // property name hobe status and value name hobe amar select er value theke ja pabo tai
+    }
+    fetch(`http://localhost:5000/apply/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(data => {
+        if(data.modifiedCount){
+          Swal.fire({
+            title: "Sucess!",
+            text: "Your file has been success.",
+            icon: "success"
+          });
+        }
+      })
   }
 
   return (
@@ -33,9 +54,9 @@ export default function ViewApplication() {
                   <td>SortWare Enginner</td>
                   <td>
                     <select
-                    onChange={ (e) => handleStatusUpdate(e, app._id)}
-                    className="select select-bordered select-xs w-full max-w-xs"
-                    defaultValue={app.status || 'Change Status'}
+                      onChange={(e) => handleStatusUpdate(e, app._id)}
+                      className="select select-bordered select-xs w-full max-w-xs"
+                      defaultValue={app.status || 'Change Status'}
                     >
                       <option disabled>Change Status</option>
                       <option>Under Review</option>
